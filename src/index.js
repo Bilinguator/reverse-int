93c504f045057ab22738009be38b1465bd8bad2e
Dmitrii Snitkin <snitkin.d@list.ru>
2020-09-29 17:58:14 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n));
+  let resultStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+      resultStr += str[i];
+  }
+  return Number(resultStr);
 }
